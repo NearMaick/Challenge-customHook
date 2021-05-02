4fd74bd7c8aee838cdedaca27040a9ac8a23b6ce
Maick Souza <maick_a_s@msn.com>
2021-05-02 11:41:19 -0300

    fix(web): useCart.spec.tsx #5

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -61,7 +61,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       }
 
       setCart(updatedCart);
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
       toast.error('Erro na adição do produto');
     }
